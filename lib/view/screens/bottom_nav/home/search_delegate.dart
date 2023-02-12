be42e92ee1268d69fa81c87985cfe3ce16fd5f9d
personaljss <yusufemirbektas08@gmail.com>
2023-02-12 03:16:10 +0300

    added search bar

---
+++ lib/view/screens/bottom_nav/home/search_delegate.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:pay_match/model/data_models/base/Asset.dart';
+import 'package:pay_match/model/observables/stocks_model.dart';
+import 'package:pay_match/view/ui_tools/stock_card.dart';
+import 'package:provider/provider.dart';
+
+class StockSearchDelegate extends SearchDelegate {
+  StockSearchDelegate({required this.listName});
+  final String listName;
+// Demo list to show querying
+  List<Asset> searchTerms = [];
+
+// first overwrite to
+// clear the search text
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    return [
+      IconButton(
+        onPressed: () {
+          query = '';
+        },
+        icon: Icon(Icons.clear),
+      ),
+    ];
+  }
+
+// second overwrite to pop out of search menu
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return IconButton(
+      onPressed: () {
+        close(context, null);
+      },
+      icon: Icon(Icons.arrow_back),
+    );
+  }
+
+// third overwrite to show query result
+  @override
+  Widget buildResults(BuildContext context) {
+    searchTerms=context.select<StocksModel,List<Asset>>((model)=>model.allAssets);
+    List<Asset> matchQuery = [];
+    for (Asset asset in searchTerms) {
+      if (asset.symbol.toLowerCase().contains(query.toLowerCase())) {
+        matchQuery.add(asset);
+      }
+    }
+    return ListView.builder(
+      itemCount: matchQuery.length,
+      itemBuilder: (context, index) {
+        var result = matchQuery[index];
+        return StocksCard(asset: result);
+      },
+    );
+  }
+
+// last overwrite to show the
+// querying process at the runtime
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    searchTerms=context.select<StocksModel,List<Asset>>((model)=>model.allAssets);
+    List<Asset> matchQuery = [];
+    for (Asset asset in searchTerms) {
+      if (asset.symbol.toLowerCase().contains(query.toLowerCase())) {
+        matchQuery.add(asset);
+      }
+    }
+    return ListView.builder(
+      itemCount: matchQuery.length,
+      itemBuilder: (context, index) {
+        var result = matchQuery[index];
+        return StocksCard(asset: result);
+      },
+    );
+  }
+}
